c6fa99cca6d43f874cc1f59b621fd3883fa9dcfe
smahene <smaheninou@hotmail.com>
2026-05-20 16:43:17 +0200

    fix views remove owner references

--- todos/views.py
+++ todos/views.py
@@ -1,17 +1,16 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from django.http import JsonResponse
 from .models import Todo
 from .serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
-    # permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Todo.objects.filter(owner=self.request.user).order_by('-created_at')
+        return Todo.objects.all().order_by('-created_at')
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        serializer.save()
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
